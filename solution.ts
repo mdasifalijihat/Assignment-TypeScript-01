65be803ef31e75eba32b6aaa45e49887909daed6
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-18 12:09:16 +0600

    added problem 2 solved

--- solution.ts
+++ solution.ts
@@ -11,3 +11,12 @@ function formatValue(
 }
 
 
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
+}
+
